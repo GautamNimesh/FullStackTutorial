b35bab6c0b023b06c3dc328ad3d4098079a2c628
GautamNimesh <np03cs4a240001@heraldcollege.edu.np>
2026-01-25 19:08:57 +0545

    Workshop 10 Submitted

---
+++ dashboard.php
@@ -0,0 +1,38 @@
+<?php
+
+session_start();
+
+require 'db.php';
+
+$user_email = '';
+
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+    $sql = "SELECT email FROM users WHERE id = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt-> execute([$user_id ]);
+    $user = $stmt->fetch();
+    if ($user) {
+        $user_email = $user['email'];
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+
+<h1>Welcome to my site</h1>
+<?php if ($user_email): ?>
+    <p>Logged In User : <?php echo htmlspecialchars($user_email); ?></p>
+<?php endif; ?>
+
+<a href="logout.php">
+    <button>Logout</button>
+</a>
+
+</body>
+</html>
